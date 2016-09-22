e92ef014f4e1e798eebb5ecb7c5bfe89cc00dfe1
Miguel Pedrero <miguel.servimarket@gmail.com>
2016-09-21 22:29:40 -0300

    ultimo

--- p5/main.js
+++ p5/main.js
@@ -9,12 +9,32 @@ $('body').keydown(function(e){
 	if(e.keyCode == 37) {//left
 		console.log("left!!");
 		$('div').css({'left':contador+'px'});
-		contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+		contador --;contador --;contador --;contador --;contador --;
+
+
 
 	} else if(e.keyCode == 39){//right
 		
 		$('div').css({'left':contador});
-		contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
+		contador++;contador++;contador++;contador++;contador++;
 
 	}
 
